744dcdd6698f1d1e6e8d013eb01274ec18cff14b
StijnWoestenborghs <stijn.woestenborghs@live.be>
2024-06-16 14:26:12 +0200

    include footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
                     <Routes>
                         <Route path='/' element={<Home />} />
                     </Routes>
+                    <Footer />
                 </Router>
             </div>
         </>
